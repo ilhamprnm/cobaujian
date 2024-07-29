72b94dd3c667fd204c079ff8ce9678e27a575497
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2024-07-29 09:50:32 +0700

    add questions file

---
+++ src/data/contohSoal.js
@@ -0,0 +1,664 @@
+const soal1 = [
+  {
+    No:1,
+    Soal:"Hari lahir lancasila diperingati setiap tanggal?",
+    JawabanSalah: [
+      "17 agustus", 
+      "18 agustus", 
+      "1 juli", 
+      "25 maret"
+    ],
+    Jawaban:"1 juni",
+  }, 
+  {
+    No:2,
+    Soal:"Nilai-nilai yang terkandung dalam pancasila antara lain materiil, kerohanian, dan vital. Hal itu merupakan pendapat?",
+    JawabanSalah: [
+      "soekarno", 
+      "M hatta", 
+      "M Yamin", 
+      "Dr Radjiman"
+    ],
+    Jawaban:"Prof Notonegoro",
+  },
+  {
+    No:3,
+    Soal:"Lambang pada Garuda Pancasila yang melambangkan kemanusiaan. yang adil dan beradab adalah?",
+    JawabanSalah: [
+      "bintang emas", 
+      "pohon beringin", 
+      "kepala banteng", 
+      "padi dan kapas"
+    ],
+    Jawaban:"rantai emas",
+  },
+  {
+    No:4,
+    Soal:"Pancasila memiliki kemampuan untuk menyesuaikan diri dengan perkembangan zaman merupakan ideologi pancasila dilihat dari dimensi?",
+    JawabanSalah: [
+      "kaidah", 
+      "realita", 
+      "fundamental", 
+      "idealisme"
+    ],
+    Jawaban:"fleksibilitas",
+  },
+  {
+    No:5,
+    Soal:"Piagam jakarta merupakan rancangan dasar pembukaan undang-undang dasar 1945 yang disahkan oleh?",
+    JawabanSalah: [
+      "ppki", 
+      "presiden soekarno", 
+      "pemerintah jepang", 
+      "pemerintah belanda"
+    ],
+    Jawaban:"bpupki",
+  },
+  {
+    No:6,
+    Soal:"Dasar hukum pembentukan mahkamah konstitusi adalah?",
+    JawabanSalah: [
+      "UU No 24 tahun 2004", 
+      "UU No 23 tahun 2003", 
+      "UU No 23 tahun 2005", 
+      "UU No 25 tahun 2005"
+    ],
+    Jawaban:"UU No 24 tahun 2003",
+  },
+  {
+    No:7,
+    Soal:"Amandemen UUD 1945 yang disahkan pada 9 November 2001 mengatur tentang?",
+    JawabanSalah: [
+      "pembatasan masa jabatan presiden dan wakil presiden", 
+      "garis besar haluan negara (GBHN)", 
+      "hubungan luar negeri indonesia", 
+      "kebijakan ekonomi indonesia"
+    ],
+    Jawaban:"tata cara pemilihan presiden dan wakil presiden, serta pemakzulan presiden",
+  },
+  {
+    No:8,
+    Soal:"Penyelenggaraan yang bersih dan bebas korupsi, kolusi dan nepotisme (KKN) sesuai dengan undang-undang nomor?",
+    JawabanSalah: [
+      "18 tahun 2000", 
+      "34 tahun 1999", 
+      "32 tahun 1999", 
+      "22 tahun 1999"
+    ],
+    Jawaban:"28 tahun 1999",
+  },
+  {
+    No:9,
+    Soal:"Dasar hukum pembentukan mahkamah konstitusi adalahsuatu batas laut yang ditarik dari sebuah garis dasar dengan jarak 12 mil ke arah laut disebut batas?",
+    JawabanSalah: [
+      "batas kedaulatan", 
+      "daerah pabean", 
+      "zona ekonomi eksklusif (ZEE)", 
+      "landas kontinen"
+    ],
+    Jawaban:"laut teritorial",
+  },
+  {
+    No:10,
+    Soal:"pasukan PETA (Pembela Tanah Air) didirikan pada?",
+    JawabanSalah: [
+      "8 januari 1944", 
+      "9 maret 1943", 
+      "5 september 1943", 
+      "1 maret 1945"
+    ],
+    Jawaban:"3 oktober 1943",
+  },
+  {
+    No:11,
+    Soal:"komisi PBB yang membantu menyelesaikan perselisihan antara indonesia dan belanda atas wilayah irian barat adalah?",
+    JawabanSalah: [
+      "UNTEA", 
+      "KTN", 
+      "AFNEI", 
+      "UNESCO"
+    ],
+    Jawaban:"UNCI",
+  }, 
+  {
+    No:12,
+    Soal:"tujuan negara republik indonesia yang berkaitan dengan duni internasional adalah?",
+    JawabanSalah: [
+      "mencerdaskan kehidupan bangsa", 
+      "memajukan kesejahteraan umum", 
+      "melindungi segena bangsa dan seluruh tumpah darah", 
+      "memajukan kerjasama regional"
+    ],
+    Jawaban:"melaksanakan ketertiban dunia",
+  },
+  {
+    No:13,
+    Soal:"rumah adat tongkonan berasal dari provinsi?",
+    JawabanSalah: [
+      "Maluku", 
+      "Sulawesi Tenggara", 
+      "Sulawesi Tengah", 
+      "Sulawesi Utara"
+    ],
+    Jawaban:"Sulawesi Selatan",
+  },
+  {
+    No:14,
+    Soal:"Negara yang termasuk dalam blok fasis dalam perang dunia II adalah?",
+    JawabanSalah: [
+      "inggris", 
+      "perancis", 
+      "polandia", 
+      "belgia"
+    ],
+    Jawaban:"austria",
+  },
+  {
+    No:15,
+    Soal:"sungai terpanjang di indonesia adalah?",
+    JawabanSalah: [
+      "sungai musi", 
+      "sungan bengawan solo", 
+      "sungai mahakam", 
+      "sungai kampar"
+    ],
+    Jawaban:"sungai kapuas",
+  },
+  {
+    No:16,
+    Soal:"Bhinneka tunggal ika sebagai penjelmaan persatuan dan kesatuan bangsa (nasionalisme) diatur dalam?",
+    JawabanSalah: [
+      "PP No 67 tahun 1951", 
+      "PP No 68 tahun 1951", 
+      "PP No 69 tahun 1951", 
+      "PP No 70 tahun 1951"
+    ],
+    Jawaban:"PP No 66 tahun 1951",
+  },
+  {
+    No:17,
+    Soal:"simbol garuda pancasila pertama kali digunakan pada tanggal?",
+    JawabanSalah: [
+      "17 agustus 1945", 
+      "22 maret 1978", 
+      "18 agustus 1945", 
+      "9 agustus 1945"
+    ],
+    Jawaban:"11 februari 1950",
+  },
+  {
+    No:18,
+    Soal:"Pancasila sebagai sebuah kesepakatan, perjanjian, serta konsensus bangsa indonesia sebagai dasar negara merupakan kedudukan pancasila sebagai?",
+    JawabanSalah: [
+      "kepribadian bangsa", 
+      "pandangan hidup", 
+      "perjanjian luhur", 
+      "jiwa bangsa"
+    ],
+    Jawaban:"cita-cita dan tujuan bangsa",
+  },
+  {
+    No:19,
+    Soal:"keselarasan dan keseimbangan adalah ciri khas dari ideologi pancasila, artinya?",
+    JawabanSalah: [
+      "pancasila mengajarkan pola hidup sehat", 
+      "kesejahteraan bagi seluruh rakyat", 
+      "ketuhanan yang maha esa", 
+      "keadilan sosial yang merata bagi seluruh rakyat indonesia"
+    ],
+    Jawaban:"pancasila memberikan panduan hidup bermasyarakat, bernegara, bahkan beragama yang baik",
+  },
+  {
+    No:20,
+    Soal:"Menurut pasal 1 ayat (1) UUD 1945, negara indonesia adalah negara kesatuan yang berbentuk?",
+    JawabanSalah: [
+      "monarki", 
+      "demokrasi", 
+      "parlementer", 
+      "presidensial"
+    ],
+    Jawaban:"republik",
+  },
+  {
+    No:21,
+    Soal:" Kepemimpinan nasional berlangsung secara periodik lima tahun sekali. Regulasi tersebut didasarkan dalam UUD 1945?",
+    JawabanSalah: [
+      "bab III pasal 4 ayat (1)", 
+      "bab I pasal 1 ayat (2)", 
+      "bab IV pasal 18", 
+      "bab II pasal 2 ayat (2)"
+    ],
+    Jawaban:"bab IV pasal 16 ayat (2)",
+  }, 
+  {
+    No:22,
+    Soal:"pembukaan UUD 1945 tercantum dalam piagam Jakarta disahkan pada tanggal?",
+    JawabanSalah: [
+      "17 agustus 1945", 
+      "14 juni 1945", 
+      "1 juni 1955", 
+      "29 mei 1945"
+    ],
+    Jawaban:"22 juni 1945",
+  },
+  {
+    No:23,
+    Soal:"pernyataan yang bukan merupakan makna yang terkandung dalam pembukaan UUD 1945 alinea pertama adalah?",
+    JawabanSalah: [
+      "anti-penjajahan", 
+      "kemerdekaan adalah hak semua bangsa", 
+      "bangsa indonesia bertekad untuk merdeka", 
+      "berjuang menentang setiap bentuk penjajahan dan mendukung kemerdekaan setiap bangsa"
+    ],
+    Jawaban:"pengukuhan atas proklamasi kemerdekaan",
+  },
+  {
+    No:24,
+    Soal:"di bawah ini bukan merupakan teori asal mula terjadinya negara adalah?",
+    JawabanSalah: [
+      "teori keutuhan", 
+      "teori kekuasaan", 
+      "teori pejanjian masyarakat", 
+      "teori hukum alam"
+    ],
+    Jawaban:"teori kedaulatan",
+  },
+  {
+    No:25,
+    Soal:" perlawanan terhadap penjajahan yang dipimpin oleh imam bonjol adalah?",
+    JawabanSalah: [
+      "perang rakyat maluku", 
+      "perang puputan", 
+      "perang banjar", 
+      "perang tapanuli"
+    ],
+    Jawaban:"perang paderi",
+  },
+  {
+    No:26,
+    Soal:"isi perjanjian renville adalah sebagai berikut, kecuali?",
+    JawabanSalah: [
+      "akan dibentuk dewan konstitusi RIS dalam waktu dekat", 
+      "akan diadakan pemilu", 
+      "RI merupakan bagian dari RIS", 
+      "TNI harus hijarah ke RI"
+    ],
+    Jawaban:"Belanda hanya mengakui Jawa sebagai wilayah RI",
+  },
+  {
+    No:27,
+    Soal:"hasil perundingan linggarjati menyatakan bahwa wilayah kekuasaan Indonesia menjadi?",
+    JawabanSalah: [
+      "sumatra, jawa, dan kalimantan", 
+      "sumatra, jawa, dan bali", 
+      "sumatra dan jawa", 
+      "Jawa saja"
+    ],
+    Jawaban:"sumatra, jawa, dan madura",
+  },
+  {
+    No:28,
+    Soal:"tari maengket adalah tari khas daerah?",
+    JawabanSalah: [
+      "bali", 
+      "belitung", 
+      "nusa tenggara barat", 
+      "jawa timur"
+    ],
+    Jawaban:"sulawesi utara",
+  },
+  {
+    No:29,
+    Soal:"pasal dalam UUD 1945 yang berkaitan dengan bhineka tunggal ika, kecuali?",
+    JawabanSalah: [
+      "pasal 6A ayat (3)", 
+      "pasal 18, 18A, dan 18B", 
+      "pasal 25A", 
+      "pasal 26 ayat (1)"
+    ],
+    Jawaban:"pasal 29 ayat (1)",
+  },
+  {
+    No:30,
+    Soal:"hak kewarganegaraan yang diperoleh sesorang berdasarkan kewarganegaraan ayah dan ibu biologisnya disebut?",
+    JawabanSalah: [
+      "ius soli", 
+      "stelsel aktif", 
+      "stelsel pasif", 
+      "repudiasi"
+    ],
+    Jawaban:"ius sanguinis",
+  },
+  {
+    No:31,
+    Soal:"Madya =",
+    JawabanSalah: [
+      "umum", 
+      "tinggi", 
+      "sedang", 
+      "rendah"
+    ],
+    Jawaban:"tengah",
+  }, 
+  {
+    No:32,
+    Soal:"komandan =",
+    JawabanSalah: [
+      "utama", 
+      "pasukan", 
+      "satu", 
+      "perang"
+    ],
+    Jawaban:"kepala",
+  },
+  {
+    No:33,
+    Soal:"Narasi =",
+    JawabanSalah: [
+      "sastra", 
+      "ajakan", 
+      "runtutan", 
+      "tema"
+    ],
+    Jawaban:"kejadian",
+  },
+  {
+    No:34,
+    Soal:"signifikan =",
+    JawabanSalah: [
+      "banyak", 
+      "jauh", 
+      "tinggi", 
+      "baik"
+    ],
+    Jawaban:"penting",
+  },
+  {
+    No:35,
+    Soal:"dinamis =",
+    JawabanSalah: [
+      "cepat", 
+      "aktif", 
+      "tetap", 
+      "diam"
+    ],
+    Jawaban:"semangat",
+  },
+  {
+    No:36,
+    Soal:"implikasi =",
+    JawabanSalah: [
+      "sebab", 
+      "akibat", 
+      "tepat", 
+      "aturan"
+    ],
+    Jawaban:"terlibat",
+  },
+  {
+    No:37,
+    Soal:"makna =",
+    JawabanSalah: [
+      "kuno", 
+      "setia", 
+      "bangga", 
+      "tujuan"
+    ],
+    Jawaban:"11 februari 1950",
+  },
+  {
+    No:38,
+    Soal:"nuansa =",
+    JawabanSalah: [
+      "berbeda", 
+      "suasana", 
+      "indah", 
+      "kemampuan"
+    ],
+    Jawaban:"variasi",
+  },
+  {
+    No:39,
+    Soal:"boikot =",
+    JawabanSalah: [
+      "menerima", 
+      "memutus", 
+      "mencaci", 
+      "membuang"
+    ],
+    Jawaban:"menolak",
+  },
+  {
+    No:40,
+    Soal:"jumbai =",
+    JawabanSalah: [
+      "besar", 
+      "raksasa", 
+      "urai", 
+      "kecil"
+    ],
+    Jawaban:"rumbai",
+  },
+
+  {
+    No:41,
+    Soal:"bebas ><",
+    JawabanSalah: [
+      "bergerak", 
+      "leluasa", 
+      "takut", 
+      "terbatas"
+    ],
+    Jawaban:"terhalang",
+  }, 
+  {
+    No:42,
+    Soal:"lunglai ><",
+    JawabanSalah: [
+      "besar", 
+      "baja", 
+      "lemah", 
+      "lelah"
+    ],
+    Jawaban:"kuat",
+  },
+  {
+    No:43,
+    Soal:"botani ><",
+    JawabanSalah: [
+      "tumbuhan", 
+      "hewan", 
+      "alam", 
+      "herbal"
+    ],
+    Jawaban:"nabati",
+  },
+  {
+    No:44,
+    Soal:"brilian ><",
+    JawabanSalah: [
+      "cerdas", 
+      "kaya", 
+      "miskin", 
+      "lemah"
+    ],
+    Jawaban:"dungu",
+  },
+  {
+    No:45,
+    Soal:"kontan ><",
+    JawabanSalah: [
+      "cicil", 
+      "piutang", 
+      "bayar", 
+      "lunas"
+    ],
+    Jawaban:"hutang",
+  },
+  {
+    No:46,
+    Soal:"konveks ><",
+    JawabanSalah: [
+      "kembung", 
+      "cembung", 
+      "gelembung", 
+      "jahit"
+    ],
+    Jawaban:"konkaf",
+  },
+  {
+    No:47,
+    Soal:"kredit ><",
+    JawabanSalah: [
+      "pengeluaran", 
+      "pencicilan", 
+      "pembayaran", 
+      "pembelian"
+    ],
+    Jawaban:"pemasukan",
+  },
+  {
+    No:48,
+    Soal:"krisis ><",
+    JawabanSalah: [
+      "labil", 
+      "tepat", 
+      "moneter", 
+      "aman"
+    ],
+    Jawaban:"stabil",
+  },
+  {
+    No:49,
+    Soal:"fisik ><",
+    JawabanSalah: [
+      "tubuh", 
+      "gerak", 
+      "terlihat", 
+      "pikir"
+    ],
+    Jawaban:"mental",
+  },
+  {
+    No:50,
+    Soal:"begah ><",
+    JawabanSalah: [
+      "gendut", 
+      "isi", 
+      "mulas", 
+      "kenyang"
+    ],
+    Jawaban:"lapar",
+  },
+
+  {
+    No:51,
+    Soal:"oplet : kendaraan = ... : kayu",
+    JawabanSalah: [
+      "kusen", 
+      "dipan", 
+      "kursi", 
+      "gergaji"
+    ],
+    Jawaban:"cendana",
+  }, 
+  {
+    No:52,
+    Soal:"peci : kepala = ... : tangan",
+    JawabanSalah: [
+      "lengan", 
+      "kalung", 
+      "kain", 
+      "ikat pinggang"
+    ],
+    Jawaban:"gelang",
+  },
+  {
+    No:53,
+    Soal:"manusia : rumah = macan : ...",
+    JawabanSalah: [
+      "pohon", 
+      "ayam", 
+      "kulit", 
+      "buas"
+    ],
+    Jawaban:"hutan",
+  },
+  {
+    No:54,
+    Soal:"lelah : aktivitas = obesitas : ...",
+    JawabanSalah: [
+      "gemuk", 
+      "tensi", 
+      "gula", 
+      "jelek"
+    ],
+    Jawaban:"lemak",
+  },
+  {
+    No:55,
+    Soal:"bayi : tua = kandungan : ...",
+    JawabanSalah: [
+      "lahir", 
+      "dewasa", 
+      "pandai", 
+      "dosa"
+    ],
+    Jawaban:"dunia",
+  },
+  {
+    No:56,
+    Soal:"ikal : keriting = kredit : ...",
+    JawabanSalah: [
+      "bank", 
+      "debit", 
+      "dunia", 
+      "dosa"
+    ],
+    Jawaban:"cicil",
+  },
+  {
+    No:57,
+    Soal:"raja : permaisuri = papan tulis : ...",
+    JawabanSalah: [
+      "pensil", 
+      "penghapus", 
+      "hitam", 
+      "belajar"
+    ],
+    Jawaban:"kapur",
+  },
+  {
+    No:58,
+    Soal:"detik : waktu = ... : berat",
+    JawabanSalah: [
+      "gram", 
+      "massa", 
+      "newton", 
+      "joule"
+    ],
+    Jawaban:"kilogram",
+  },
+  {
+    No:59,
+    Soal:"apotek : obat = ... : baju",
+    JawabanSalah: [
+      "toko", 
+      "swalayan", 
+      "pasar", 
+      "salon"
+    ],
+    Jawaban:"butik",
+  },
+  {
+    No:60,
+    Soal:"tinta : pena : tulisan",
+    JawabanSalah: [
+      "obeng : baut : paku", 
+      "keset : sepatu : kaos kaki", 
+      "jalan : gapura : kampung", 
+      "helm : topi : sepatu"
+    ],
+    Jawaban:"loyang : pemanggang : roti",
+  },
+]
